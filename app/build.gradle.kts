0a0b12e08fe505c3b6a2a535ef012baf4426e2ae
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2025-04-21 14:30:43 +0800

    Change versionName to match GitHub release name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "moe.chensi.volume"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
