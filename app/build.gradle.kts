debb897bcb22a9ca3c21b082d643f62cab799e7f
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2025-09-28 01:04:12 +0800

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "moe.chensi.volume"
         minSdk = 33
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3-beta.1"
+        versionCode = 6
+        versionName = "0.3-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
