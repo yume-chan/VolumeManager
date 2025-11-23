7ccf70d7c9a0013595135ccbde79e6356e978ca1
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2025-11-23 14:08:21 +0800

    Increment versionCode from 10 to 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         applicationId = "moe.chensi.volume"
         minSdk = 33
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         versionName = "0.3-beta.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
